ac825c79a9c9a28346e8c1ae7b8d9ffd631702c1
moshixd <72013530+moshixd@users.noreply.github.com>
2022-02-09 10:32:35 +0100

    Add files via upload
    Examensarbete WIP

---
+++ GymLog/src/main/java/net/moshi/gymlog/GymLogApplication.java
@@ -0,0 +1,13 @@
+package net.moshi.gymlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymLogApplication.class, args);
+    }
+
+}
---
+++ GymLog/src/test/java/net/moshi/gymlog/GymLogApplicationTests.java
@@ -0,0 +1,13 @@
+package net.moshi.gymlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
